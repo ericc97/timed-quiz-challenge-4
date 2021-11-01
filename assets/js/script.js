11b13ae67cf4205fd47a2d1fcea5434950a49022
Eric Coleman <ericcoleman9217@gmail.com>
2021-11-01 16:40:57 -0500

    added first 3 questions to the quiz

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+// 1. Welcome to the timed quiz!
+// 2. A 75 second timer will run throughout the quiz.
+// 3. If you get a question right, the next question will appear.
+// 4. If you get a question wrong, time will be ducted from the remaining time and the next question will appear. 
+// 5. Score is calculated based off of the time remaining at the end.
+// 6. Your final score will be added to a list of high scores!
+
+
+
+// What does HTML stand for?
+// Hyper Text Math Law
+// Hyper Text Markup Language
+// Hyper Text Multi Language
+// Hyper Text Macro Language
+
+// What is the purpose of css?
+// To Alter the Styling and Overview of the HTML 
+// To add more HTML
+// To change the order in which the html is read
+// To allow hackers access to the code file
+
+// What is Software Engineering?
+// Designing a software
+// Testing a software
+// Application of engineering principles to the design a software
+// None of the above
+
